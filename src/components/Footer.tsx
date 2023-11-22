e16e058df3531cdda069d669d54c26f1d6ff14bb
S1NS3RO <douglassgbr@gmail.com>
2023-11-22 15:49:36 -0300

    Preparação básica

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import '@/styles/c-footer.scss'
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className='footer-container'>&copy; Douglas Vargas e Vithor Silva</div>
+    </footer>
+  )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import '@/styles/c-header.scss'
+
+export default function Header() {
+  return (
+    <header>
+      <div className="header-container">header</div>
+    </header>
+  )
+}
