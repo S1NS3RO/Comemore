867528a716f38a957f2093534dccc55aea25fb92
S1NS3RO <douglassgbr@gmail.com>
2023-11-22 16:22:44 -0300

    styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,16 @@ import '@/styles/c-header.scss'
 export default function Header() {
   return (
     <header>
-      <div className="header-container">header</div>
+      <div className='header-container'>
+        <div className="logo">Comemore</div>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>Login</li>
+            <li>Comprar</li>
+          </ul>
+        </nav>
+      </div>
     </header>
   )
 }
