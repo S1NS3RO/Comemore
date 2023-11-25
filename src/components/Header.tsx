27659c2b70f7f9646909ff745a79ca1746393ead
S1NS3RO <douglassgbr@gmail.com>
2023-11-25 00:19:50 -0300

    Menu mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,21 +2,36 @@
 import '@/styles/c-header.scss'
 
 import Link from 'next/link'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
+import { HeaderNav } from './HeaderNav.tsx'
 import { CiLogin } from 'react-icons/ci'
 import { IoMenu } from 'react-icons/io5'
 
 export default function Header() {
   const [isMobile, setIsMobile] = useState(false)
   const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const menuRef = useRef(null)
+
+  useEffect(() => {
+    const handleCloseMenu = event => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setIsMenuOpen(false)
+      }
+    }
+    document.addEventListener('mousedown', handleCloseMenu)
+    return () => {
+      document.removeEventListener('mousedown', handleCloseMenu)
+    }
+  }, [menuRef])
 
   useEffect(() => {
     const updateWindowDimensions = () => {
       if (window.innerWidth <= 600) {
-        setIsMobile(false)
-      } else {
         setIsMobile(true)
+      } else {
+        setIsMobile(false)
       }
+      //setIsMenuOpen(false)
     }
 
     window.addEventListener('resize', updateWindowDimensions)
@@ -34,15 +49,9 @@ export default function Header() {
         <div className='logo'>
           <Link href='/'>Comemore</Link>
         </div>
-        {isMobile ? (
+        {!isMobile ? (
           <>
-            <nav>
-              <ul>
-                <li>
-                  <Link href='/planos'>Planos</Link>
-                </li>
-              </ul>
-            </nav>
+            <HeaderNav />
             <div className='profile'>
               <Link href='/login'>
                 <CiLogin /> Login
@@ -54,16 +63,14 @@ export default function Header() {
         )}
       </div>
       {isMenuOpen && (
-        <div className='menu-open'>
-          <h3>Menu</h3>
-          <ul>
-            <li>
-              <Link href='/planos'>Planos</Link>
-            </li>
-          </ul>
+        <div
+          ref={menuRef}
+          className='menu-open'>
+          <h3>Bem Vindo</h3>
+          <HeaderNav />
           <div className='profile'>
             <Link href='/login'>
-              <CiLogin /> Login
+              <CiLogin />{' '}Login
             </Link>
           </div>
         </div>
