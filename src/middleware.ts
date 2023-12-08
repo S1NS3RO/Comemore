3d2b577e0057d9f13bb61f57e19324028300fcb1
Douglas Vargas <75233972+S1NS3RO@users.noreply.github.com>
2023-12-08 09:36:36 -0300

    Update middleware.ts
    Adicionado imagens para exibição sem precisar de autenticação

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,13 @@
 import { NextRequest, NextResponse } from 'next/server'
 import AuthService from './modules/auth/services/auth-services'
 
+// export const config = {
+//   matcher: '/((?!_next/static|_next/image|favicon.ico).*)'
+// }
 export const config = {
-  matcher: '/((?!_next/static|_next/image|favicon.ico).*)'
+  matcher: '/((?!(_next/static|_next/image|favicon.ico|\\.png|\\.jpg|\\.jpeg|\\.gif)).*)'
 }
+
 
 // Lista de rotas que não exigem login
 const publicRoutes = ['/', '/login/', '/cadastro/']
