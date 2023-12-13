2bfeefd78065a9392d9fb7498523d67bb2007782
Douglas Vargas <75233972+S1NS3RO@users.noreply.github.com>
2023-12-13 11:14:36 -0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import AuthService from './modules/auth/services/auth-services'
 
 export const config = {
-  matcher: '/^(?!.*\/_next\/(?:static|image)|favicon.ico).*$/'
+  matcher: '/((?!_next/static|_next/image*|favicon.ico).*)'
 }
 
 // Lista de rotas que não exigem login
