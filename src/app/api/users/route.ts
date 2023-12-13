90929987a998ac2016717f4dee7f38ed48ca161a
Douglas Vargas <75233972+S1NS3RO@users.noreply.github.com>
2023-12-13 10:15:22 -0300

    Update route.ts

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -5,12 +5,8 @@ const prisma = new PrismaClient()
 export async function GET() {
   const users = await prisma.user.findMany({
     select: {
-      id: true,
       name: true,
       email: true,
-      password: true,
-      createdAt: true,
-      updatedAt: true,
       role: true
     }
   })
